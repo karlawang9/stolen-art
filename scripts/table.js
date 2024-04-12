a54e3a040df28a879470065e152297050b03e20e
Karla Wang <12258+kiw9@users.noreply.github.coecis.cornell.edu>
2024-04-11 20:13:28 -0400

    import files

---
+++ scripts/table.js
@@ -0,0 +1,33 @@
+const table = d3.select("#table table");
+var tbody = table.append("tbody");
+
+const requestTableData = async function() {
+
+    const artData = await d3.csv("../data/interpol_country_data.csv"); // no autotype, need country codes as strings
+    artData.forEach( (d, i) => {
+        d.total = Number(d.total); // now convert totals to numeric data
+    });
+
+    // create array of key-value pairs
+    var items = []
+    artData.forEach (d => {
+        items.push([d.country_name, d.total]);
+    });
+    
+    // sort array based on values
+    items.sort(
+        (first, second) => { return second[1] - first[1] } 
+    );
+
+    for (i=0; i<10; i++) {
+        country_name = items[i][0];
+        total = items[i][1];
+
+        var tr = tbody.append("tr");
+        tr.append("td").text(country_name);
+        tr.append("td").text(d3.format(",")(total));
+    };
+
+};
+
+requestTableData();
